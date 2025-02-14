6b26bbfd0ccce46d5a4e39c7ca82d0b1a3b4a646
Arseniy <arseniy.khavanov@gmail.com>
2025-02-14 15:27:03 +0500

    added tests for transactionService

---
+++ internal/service/transaction_test.go
@@ -0,0 +1,284 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/senyabanana/avito-shop-service/internal/entity"
+	mocks "github.com/senyabanana/avito-shop-service/internal/repository/mocks"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	trmsqlx "github.com/avito-tech/go-transaction-manager/drivers/sqlx/v2"
+	"github.com/avito-tech/go-transaction-manager/trm/v2/manager"
+	"github.com/golang/mock/gomock"
+	"github.com/jmoiron/sqlx"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTransactionService_GetUserInfo(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUserRepo := mocks.NewMockUserRepository(ctrl)
+	mockTransactionRepo := mocks.NewMockTransactionRepository(ctrl)
+	mockInventoryRepo := mocks.NewMockInventoryRepository(ctrl)
+	db, mock, _ := sqlmock.New()
+	mockDB := sqlx.NewDb(db, "sqlmock")
+	mockTrManager := manager.Must(trmsqlx.NewDefaultFactory(mockDB))
+	mockLog := logrus.New()
+
+	service := NewTransactionService(mockUserRepo, mockTransactionRepo, mockInventoryRepo, mockTrManager, mockLog)
+
+	tests := []struct {
+		name         string
+		mockBehavior func()
+		userID       int
+		wantInfo     entity.InfoResponse
+		wantErr      error
+	}{
+		{
+			name:   "Success",
+			userID: 1,
+			mockBehavior: func() {
+				mock.ExpectBegin()
+				mockUserRepo.EXPECT().GetUserBalance(gomock.Any(), 1).Return(100, nil)
+				mockInventoryRepo.EXPECT().GetUserInventory(gomock.Any(), 1).Return([]entity.InventoryItem{}, nil)
+				mockTransactionRepo.EXPECT().GetReceivedTransactions(gomock.Any(), 1).Return([]entity.TransactionDetail{}, nil)
+				mockTransactionRepo.EXPECT().GetSentTransactions(gomock.Any(), 1).Return([]entity.TransactionDetail{}, nil)
+				mock.ExpectCommit()
+			},
+			wantInfo: entity.InfoResponse{
+				Coins:       100,
+				Inventory:   []entity.InventoryItem{},
+				CoinHistory: entity.CoinHistory{Received: []entity.TransactionDetail{}, Sent: []entity.TransactionDetail{}},
+			},
+			wantErr: nil,
+		},
+		{
+			name:   "Error fetching balance",
+			userID: 2,
+			mockBehavior: func() {
+				mock.ExpectBegin()
+				mockUserRepo.EXPECT().GetUserBalance(gomock.Any(), 2).Return(0, errors.New("db error"))
+				mock.ExpectRollback()
+			},
+			wantInfo: entity.InfoResponse{},
+			wantErr:  errors.New("db error"),
+		},
+		{
+			name:   "Error fetching inventory",
+			userID: 3,
+			mockBehavior: func() {
+				mock.ExpectBegin()
+				mockUserRepo.EXPECT().GetUserBalance(gomock.Any(), 3).Return(100, nil)
+				mockInventoryRepo.EXPECT().GetUserInventory(gomock.Any(), 3).Return(nil, errors.New("inventory error"))
+				mock.ExpectRollback()
+			},
+			wantInfo: entity.InfoResponse{},
+			wantErr:  errors.New("inventory error"),
+		},
+		{
+			name:   "Error fetching received transactions",
+			userID: 4,
+			mockBehavior: func() {
+				mock.ExpectBegin()
+				mockUserRepo.EXPECT().GetUserBalance(gomock.Any(), 4).Return(100, nil)
+				mockInventoryRepo.EXPECT().GetUserInventory(gomock.Any(), 4).Return([]entity.InventoryItem{}, nil)
+				mockTransactionRepo.EXPECT().GetReceivedTransactions(gomock.Any(), 4).Return(nil, errors.New("received transactions error"))
+				mock.ExpectRollback()
+			},
+			wantInfo: entity.InfoResponse{},
+			wantErr:  errors.New("received transactions error"),
+		},
+		{
+			name:   "Error fetching sent transactions",
+			userID: 5,
+			mockBehavior: func() {
+				mock.ExpectBegin()
+				mockUserRepo.EXPECT().GetUserBalance(gomock.Any(), 5).Return(100, nil)
+				mockInventoryRepo.EXPECT().GetUserInventory(gomock.Any(), 5).Return([]entity.InventoryItem{}, nil)
+				mockTransactionRepo.EXPECT().GetReceivedTransactions(gomock.Any(), 5).Return([]entity.TransactionDetail{}, nil)
+				mockTransactionRepo.EXPECT().GetSentTransactions(gomock.Any(), 5).Return(nil, errors.New("sent transactions error"))
+				mock.ExpectRollback()
+			},
+			wantInfo: entity.InfoResponse{},
+			wantErr:  errors.New("sent transactions error"),
+		},
+		{
+			name:   "User has empty inventory and transactions",
+			userID: 6,
+			mockBehavior: func() {
+				mock.ExpectBegin()
+				mockUserRepo.EXPECT().GetUserBalance(gomock.Any(), 6).Return(50, nil)
+				mockInventoryRepo.EXPECT().GetUserInventory(gomock.Any(), 6).Return(nil, nil)
+				mockTransactionRepo.EXPECT().GetReceivedTransactions(gomock.Any(), 6).Return(nil, nil)
+				mockTransactionRepo.EXPECT().GetSentTransactions(gomock.Any(), 6).Return(nil, nil)
+				mock.ExpectCommit()
+			},
+			wantInfo: entity.InfoResponse{
+				Coins:       50,
+				Inventory:   []entity.InventoryItem{},
+				CoinHistory: entity.CoinHistory{Received: []entity.TransactionDetail{}, Sent: []entity.TransactionDetail{}},
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mockBehavior()
+			info, err := service.GetUserInfo(context.Background(), tt.userID)
+
+			assert.Equal(t, tt.wantErr, err)
+			assert.Equal(t, tt.wantInfo, info)
+		})
+	}
+}
+
+func TestTransactionService_SendCoin(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	// Создаем моки
+	mockUserRepo := mocks.NewMockUserRepository(ctrl)
+	mockTransactionRepo := mocks.NewMockTransactionRepository(ctrl)
+
+	db, mock, _ := sqlmock.New()
+	mockDB := sqlx.NewDb(db, "sqlmock")
+	mockTrManager := manager.Must(trmsqlx.NewDefaultFactory(mockDB))
+
+	mockLog := logrus.New()
+	service := NewTransactionService(mockUserRepo, mockTransactionRepo, nil, mockTrManager, mockLog)
+
+	tests := []struct {
+		name         string
+		mockBehavior func()
+		fromUserID   int
+		toUsername   string
+		amount       int
+		wantErr      error
+	}{
+		{
+			name:       "Success",
+			fromUserID: 1,
+			toUsername: "recipient",
+			amount:     50,
+			mockBehavior: func() {
+				mock.ExpectBegin()
+				mockUserRepo.EXPECT().GetUser(gomock.Any(), "recipient").Return(entity.User{ID: 2, Username: "recipient"}, nil)
+				mockUserRepo.EXPECT().GetUserBalance(gomock.Any(), 1).Return(100, nil)
+				mockUserRepo.EXPECT().UpdateCoins(gomock.Any(), 1, -50).Return(nil)
+				mockUserRepo.EXPECT().UpdateCoins(gomock.Any(), 2, 50).Return(nil)
+				mockTransactionRepo.EXPECT().InsertTransaction(gomock.Any(), 1, 2, 50).Return(nil)
+				mock.ExpectCommit()
+			},
+			wantErr: nil,
+		},
+		{
+			name:       "Recipient not found",
+			fromUserID: 1,
+			toUsername: "unknownUser",
+			amount:     50,
+			mockBehavior: func() {
+				mock.ExpectBegin()
+				mockUserRepo.EXPECT().GetUser(gomock.Any(), "unknownUser").Return(entity.User{}, entity.ErrRecipientNotFound)
+				mock.ExpectRollback()
+			},
+			wantErr: entity.ErrRecipientNotFound,
+		},
+		{
+			name:       "Sender tries to send to themselves",
+			fromUserID: 1,
+			toUsername: "sender",
+			amount:     50,
+			mockBehavior: func() {
+				mock.ExpectBegin()
+				mockUserRepo.EXPECT().GetUser(gomock.Any(), "sender").Return(entity.User{ID: 1, Username: "sender"}, nil)
+				mock.ExpectRollback()
+			},
+			wantErr: entity.ErrSendThemselves,
+		},
+		{
+			name:       "Insufficient balance",
+			fromUserID: 1,
+			toUsername: "recipient",
+			amount:     200,
+			mockBehavior: func() {
+				mock.ExpectBegin()
+				mockUserRepo.EXPECT().GetUser(gomock.Any(), "recipient").Return(entity.User{ID: 2, Username: "recipient"}, nil)
+				mockUserRepo.EXPECT().GetUserBalance(gomock.Any(), 1).Return(100, nil)
+				mock.ExpectRollback()
+			},
+			wantErr: entity.ErrInsufficientBalance,
+		},
+		{
+			name:       "Error fetching sender's balance",
+			fromUserID: 1,
+			toUsername: "recipient",
+			amount:     50,
+			mockBehavior: func() {
+				mock.ExpectBegin()
+				mockUserRepo.EXPECT().GetUser(gomock.Any(), "recipient").Return(entity.User{ID: 2, Username: "recipient"}, nil)
+				mockUserRepo.EXPECT().GetUserBalance(gomock.Any(), 1).Return(0, errors.New("db error"))
+				mock.ExpectRollback()
+			},
+			wantErr: errors.New("db error"),
+		},
+		{
+			name:       "Error decreasing sender's balance",
+			fromUserID: 1,
+			toUsername: "recipient",
+			amount:     50,
+			mockBehavior: func() {
+				mock.ExpectBegin()
+				mockUserRepo.EXPECT().GetUser(gomock.Any(), "recipient").Return(entity.User{ID: 2, Username: "recipient"}, nil)
+				mockUserRepo.EXPECT().GetUserBalance(gomock.Any(), 1).Return(100, nil)
+				mockUserRepo.EXPECT().UpdateCoins(gomock.Any(), 1, -50).Return(errors.New("db error"))
+				mock.ExpectRollback()
+			},
+			wantErr: errors.New("db error"),
+		},
+		{
+			name:       "Error increasing recipient's balance",
+			fromUserID: 1,
+			toUsername: "recipient",
+			amount:     50,
+			mockBehavior: func() {
+				mock.ExpectBegin()
+				mockUserRepo.EXPECT().GetUser(gomock.Any(), "recipient").Return(entity.User{ID: 2, Username: "recipient"}, nil)
+				mockUserRepo.EXPECT().GetUserBalance(gomock.Any(), 1).Return(100, nil)
+				mockUserRepo.EXPECT().UpdateCoins(gomock.Any(), 1, -50).Return(nil)
+				mockUserRepo.EXPECT().UpdateCoins(gomock.Any(), 2, 50).Return(errors.New("db error"))
+				mock.ExpectRollback()
+			},
+			wantErr: errors.New("db error"),
+		},
+		{
+			name:       "Error inserting transaction",
+			fromUserID: 1,
+			toUsername: "recipient",
+			amount:     50,
+			mockBehavior: func() {
+				mock.ExpectBegin()
+				mockUserRepo.EXPECT().GetUser(gomock.Any(), "recipient").Return(entity.User{ID: 2, Username: "recipient"}, nil)
+				mockUserRepo.EXPECT().GetUserBalance(gomock.Any(), 1).Return(100, nil)
+				mockUserRepo.EXPECT().UpdateCoins(gomock.Any(), 1, -50).Return(nil)
+				mockUserRepo.EXPECT().UpdateCoins(gomock.Any(), 2, 50).Return(nil)
+				mockTransactionRepo.EXPECT().InsertTransaction(gomock.Any(), 1, 2, 50).Return(errors.New("db error"))
+				mock.ExpectRollback()
+			},
+			wantErr: errors.New("db error"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mockBehavior()
+			err := service.SendCoin(context.Background(), tt.fromUserID, tt.toUsername, tt.amount)
+
+			assert.Equal(t, tt.wantErr, err)
+		})
+	}
+}
